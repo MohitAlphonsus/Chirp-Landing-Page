3e479afb7646b5764fefc2b87ad9c94ca6e0c4ed
MohitAlphonsus <mohitdalphonso@gmail.com>
2023-02-20 01:10:50 +0530

    user images exported

---
+++ src/assets/index.js
@@ -0,0 +1,21 @@
+import userAvatar01 from './userAvatar01.svg';
+import userAvatar02 from './userAvatar02.svg';
+import userAvatar03 from './userAvatar03.svg';
+import userAvatar04 from './userAvatar04.svg';
+import userAvatar05 from './userAvatar05.svg';
+import userAvatar06 from './userAvatar06.svg';
+import userAvatar07 from './userAvatar07.svg';
+import userAvatar08 from './userAvatar08.svg';
+import userAvatar09 from './userAvatar09.svg';
+
+export {
+	userAvatar01,
+	userAvatar02,
+	userAvatar03,
+	userAvatar04,
+	userAvatar05,
+	userAvatar06,
+	userAvatar07,
+	userAvatar08,
+	userAvatar09,
+};
